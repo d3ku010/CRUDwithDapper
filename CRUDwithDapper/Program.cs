bc176aa12756a73d2fc514bdc0ad021d0a22a6a4
d3ku010 <firdousfiru0@gmail.com>
2025-01-23 18:13:48 +0530

    Logger added

--- CRUDwithDapper/Program.cs
+++ CRUDwithDapper/Program.cs
@@ -1,33 +1,58 @@
 using CRUDwithDapper.Data;
 using CRUDwithDapper.Models;
 using CRUDwithDapper.Repository;
+using Microsoft.Extensions.Logging.Abstractions;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
+var logger = LogManager.Setup().LoadConfigurationFromFile("NLog.config").GetCurrentClassLogger();
 
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-
-builder.Services.AddTransient<IDapperContext, DapperContext>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
-var app = builder.Build();
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+
+    logger.Debug("Starting application...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
+
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+    builder.Services.AddTransient<IDapperContext, DapperContext>();
+    builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+    var app = builder.Build();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+
+    app.MapRazorPages();
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-app.UseAuthorization();
-
-
-app.MapRazorPages();
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
-app.Run();
+catch (Exception ex)
+{
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
